62ba5f8d8c711883f98b53076afe4d940f9ee2a5
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-03-04 17:28:32 -0300

    [setup] frontend and backend folders

--- app/api.py
+++ app/api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Social media app with fast API",
@@ -10,3 +11,21 @@ app = FastAPI(
         "email": "nicolas.bouffanais.1999@gmail.com",
     },
 )
+
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+@app.get("/", tags=["root"])
+async def read_root() -> dict:
+    return {"message": "Welcome to facebook clone"}
