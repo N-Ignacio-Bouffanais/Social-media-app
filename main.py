1308c65322f9f73263c4c93beb87c56221c9d201
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-03-03 22:32:42 -0300

    [setup] setup project

---
+++ main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Social media app with fast API",
+    description="This is going to be a social media app project with Fast API and reactjs",
+    version="1.0.0",
+)
