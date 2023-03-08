e25a00c288f4725a5be0c06b822cb0956c955472
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-03-07 23:26:01 -0300

    [setup] navbar conditional

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,32 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import { LandingPage } from './pages/LandingPage'
 import Login from './pages/Login'
 import Profile from './pages/Profile'
 import Register from './pages/Register'
+import AcountConfig from './pages/AcountConfig'
 
 
 function App() {
+
+  const loggedIn:boolean = false;
   return (
     <BrowserRouter>
       <Navbar/>
       <Routes>
         <Route index element={<LandingPage/>}/>
-        <Route path='/profile' element={<Profile/>}/>
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
+        <Route path='/config' element={loggedIn ? (
+          <AcountConfig />
+        ) : (
+          <Navigate replace to={"/"} />
+        )} />
+        <Route path='/profile' element={loggedIn ? (
+          <Profile />
+        ) : (
+          <Navigate replace to={"/"} />
+        )} />
       </Routes>
     </BrowserRouter>
   )
